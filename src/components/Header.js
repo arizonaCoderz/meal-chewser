11004d7caf4cebfab5df385418f5f137cb1494f5
DvdHLee <dvdhyelee@gmail.com>
2022-03-03 00:04:42 -0700

    framework

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import React from 'react';
 import './Header.css' ;
 
 const Header = props => {
+
     return (
         <div className='header'>
-            
+            <img></img>
+            <p>Meal Chewser</p>
+            <button>Home</button>
+            <button>About</button>
         </div>
     );
 }
