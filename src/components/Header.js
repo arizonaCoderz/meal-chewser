c35f034c360c45b780bbb15afd2a3a1841f134eb
enochcho <enochcho97@hotmail.com>
2022-03-07 23:18:56 -0700

    logos + logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import './Header.css' ;
+import './Header.css';
 
 const Header = props => {
 
     return (
-        <div className='header'>
-            <img></img>
-            <p className='name'>MEAL CHEWSER</p>
-            <button className='button'>Home</button>
-            <button className='button'>About</button>
+        <div className = 'header'>
+
+        <p className = 'name' > MEAL </p>   
+        <img id = "logo" src = "/assets/Logos/MealChewser_Logo1.png" alt = "this is a egg shaped like a location pin" />
+        <p className = 'name' id="chewser" > CHEWSER </p>  
+        <button className = 'button' > Home </button> 
+        <button className = 'button' > About </button> 
         </div>
     );
 }
