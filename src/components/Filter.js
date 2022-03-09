2d246bbf073decd0ee77679f19b88bf40387e39a
DvdHLee <dvdhyelee@gmail.com>
2022-03-09 15:17:26 -0700

    filter and font update

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Filter.css';
+import "./Filter.css";
 
-const Filter = props => {
-    return (
-        <div className="filter">
-            <p>Filter</p>
-        </div>
-    );
-}
+const Filter = (props) => {
+  return (
+    <div className="filter">
+      <p>Address</p>
+      <input id="input"></input>
+      <p>Max Distance</p>
+    </div>
+  );
+};
 
 export default Filter;
