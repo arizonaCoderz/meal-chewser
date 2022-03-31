d4c59a61788ca03fef6e9bcfe5c0989f0aae4f5c
enochcho <enochcho97@hotmail.com>
2022-03-31 12:31:29 -0700

    added results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
 import './Results.css';
+import Result from './Result.js';
 
 const Results = props => {
     return (
         <div className="results">
-            <p>Results</p>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+            <div className="row">
+                <Result></Result>
+            </div>
+           
         </div>
     );
 }
