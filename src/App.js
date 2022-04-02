548854dde3d1bdc9fc7ae302debdee1adb70625c
DvdHLee <dvdhyelee@gmail.com>
2022-04-01 17:48:22 -0700

    map implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 import "./App.css";
 import Header from "./components/Header";
 import Leftside from "./components/Leftside";
 import Map from "./components/Map";
 
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+const center = {
+  lat: 39.656456,
+  lng: -99.534784,
+};
+
 function App() {
+  const {isLoaded, loadError} = useLoadScript({
+      googleMapsApiKey: "AIzaSyA9oUuwu2IcJiytz70UxvzQIAtIWD_Pskc",
+      libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+
+  
   return (
     <div className="App">
       <Header></Header>
       <div id="content">
         <Leftside></Leftside>
-        <Map></Map>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={5}
+          center={center}
+        />
       </div>
     </div>
   );
