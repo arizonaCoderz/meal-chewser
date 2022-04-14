9ef58eb8809b53f1be34b6e42ab0d279172c80b6
DvdHLee <dvdhyelee@gmail.com>
2022-04-14 13:40:52 -0700

    file restructure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 import "./App.css";
 import Header from "./components/Header";
-import Leftside from "./components/Leftside";
-import Map from "./components/Map";
-
-
-
+import Content from "./components/Content";
 
 function App() {
-  const [data, setData] = useState(["", [0,0], 0, "0"]);
-
-  const pdataHandler = value => {
-    setData(value);
-  }
 
   return (
     <div className="App">
       <Header></Header>
-      <div id="content">
-        <Leftside cdataHandler={pdataHandler}></Leftside>
-        <Map address={data[0]} price={data[1]} distance={data[2]} keyword={data[3]}></Map>
-      </div>
+      <Content></Content>
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import Leftside from "./Leftside";
+import Map from "./Map";
+import "./Content.css";
+
+const Content = (props) => {
+  const [data, setData] = useState(["", [0, 0], 0, "0"]);
+
+  const pdataHandler = (value) => {
+    setData(value);
+  };
+
+  return (
+    <div>
+      <div id="content">
+        <Leftside cdataHandler={pdataHandler}></Leftside>
+        <Map
+          address={data[0]}
+          price={data[1]}
+          distance={data[2]}
+          keyword={data[3]}
+        ></Map>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
