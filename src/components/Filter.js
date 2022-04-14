9e0f78419a42c721d49aee661891e2986805c1d2
DvdHLee <dvdhyelee@gmail.com>
2022-04-14 13:17:13 -0700

    price button updated

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,7 +8,7 @@ import Overlay from "./Overlay";
 const Filter = (props) => {
   const [sliderV, setSliderV] = useState(25);
   const [address, setAddress] = useState("");
-  const [price, setPrice] = useState([0,0]);
+  const [price, setPrice] = useState([0,1]);
   const [keyword, setKeyword] = useState("");
 
   const onSliderChangeP = (value) => {
@@ -19,32 +19,13 @@ const Filter = (props) => {
     setAddress(event.target.value);
   }
 
-  const button1Handler = value => {
-    setPrice([0,1]);
-  }
-
-  const button2Handler = value => {
-    setPrice([2,3]);
-  }
-
-  const button3Handler = value => {
-    setPrice([4,4]);
+  const buttonHandler = value => {
+    setPrice(value);
   }
 
   const onKeyWordChange = event => {
     setKeyword(event.target.value);
   }
-  
-
-  // const [overlay, setOverlay] = useState(false);
-  // // const pullOverlay = (event) => {
-  // //   //pulls up the randomized result
-  // //   setOverlay(true);
-  // // };
-  // const removeOverlay = (event) => {
-  //   //removes the overlay and backdrop
-  //   setOverlay(false);
-  // };
   
   var chewsdata = ["0", [0,0], 0, "0"];
 
@@ -54,38 +35,9 @@ const Filter = (props) => {
     chewsdata[2] = sliderV;
     chewsdata[3] = keyword;
     props.chews(chewsdata);
-    // setOverlay(true);
     console.log(address)
   }
 
-  // if (overlay === true) {
-  //   return (
-  //     <div className="filter">
-  //       <p>Address</p>
-  //       <input id="input"></input>
-  //       <div id="distance">
-  //         <p style={{ marginRight: "10px" }}>Max Distance:</p>
-  //         <p>{sliderV}</p>
-  //         <p style={{ marginLeft: "5px" }}>Miles</p>
-  //       </div>
-  //       <Slider onSliderChangeC={onSliderChangeP} />
-  //       <p>Price</p>
-  //       <div id="prices">
-  //         <PriceButton value="$"></PriceButton>
-  //         <PriceButton value="$$"></PriceButton>
-  //         <PriceButton value="$$$"></PriceButton>
-  //       </div>
-  //       <p>Cuisine</p>
-  //       <div id="cuisine"></div>
-  //       <div id="chewsbuttondiv">
-  //         <button id="chewsbutton" onClick={onChewsen}>
-  //           Chews My Meal
-  //         </button>
-  //       </div>
-  //       <Overlay onClickedBackdrop={removeOverlay}></Overlay>
-  //     </div>
-  //   );
-  // } else {
     return (
       <div className="filter">
         <p>Address</p>
@@ -98,9 +50,7 @@ const Filter = (props) => {
         <Slider onSliderChangeC={onSliderChangeP} />
         <p>Price</p>
         <div id="prices">
-          <PriceButton value="$" bstatus={button1Handler}></PriceButton>
-          <PriceButton value="$$" bstatus={button2Handler}></PriceButton>
-          <PriceButton value="$$$" bstatus={button3Handler}></PriceButton>
+          <PriceButton bstatus={buttonHandler}></PriceButton>
         </div>
         <p>Cuisine</p>
         <div id="cuisine"></div>
@@ -111,7 +61,6 @@ const Filter = (props) => {
         </div>
       </div>
     );
-  //} //for the overlay
 };
 
 export default Filter;
--- src/components/Internals/PriceButton.js
+++ src/components/Internals/PriceButton.js
@@ -3,27 +3,80 @@ import React, { useState } from "react";
 import "./PriceButton.css";
 
 const PriceButton = (props) => {
-  const [clicked, setClicked] = useState(0);
-  const [bcolor, setBorderColor] = useState("transparent");
 
-  const pbuttonClicked = event => {
-    if (clicked === 0) {
-        props.bstatus(1);
-        setClicked(1);
-        setBorderColor("#5c7cbe");
-    }
-    else {
-        props.bstatus(0);
-        setClicked(0);
-        setBorderColor("transparent");
-    }
+  const [clicked, setClicked] = useState(1);
+
+  const p1clicked = event => {
+    setClicked(1);
+    props.bstatus([0,1]);
   }
+
+  const p2clicked = event => {
+    setClicked(2);
+    props.bstatus([1,2]);
+  }
+
+  const p3clicked = event => {
+    setClicked(3);
+    props.bstatus([2,3]);
+  }
+
+  const p4clicked = event => {
+    setClicked(4);
+    props.bstatus([3,4]);
+  }
+
+  // const p5clicked = event => {
+  //   setClicked(5);
+  //   props.bstatus([4,4]);
+  // }
   
-  return (
-    <div className="priceButton">
-      <button className="pbutton" style={{borderColor: bcolor}} onClick={pbuttonClicked}>{props.value}</button>
-    </div>
-  );
+  if (clicked == 1) {
+    return (
+      <div className="pricebuttons">
+        <button className="pricebutton">
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p1clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p2clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p3clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p4clicked}/>
+        </button>
+      </div>
+    );
+  } else if (clicked == 2) {
+    return (
+      <div className="pricebuttons">
+        <button className="pricebutton">
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p1clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p2clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p3clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p4clicked}/>
+        </button>
+      </div>
+    );
+  } else if (clicked == 3) {
+    return (
+      <div className="pricebuttons">
+        <button className="pricebutton">
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p1clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p2clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p3clicked}/>
+          <img className="pbutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p4clicked}/>
+        </button>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="pricebuttons">
+        <button className="pricebutton">
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p1clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p2clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p3clicked}/>
+          <img className="whitebutton" src="/assets/Logos/dollarsign.svg" alt="this is a dollarsign icon" onClick={p4clicked}/>
+        </button>
+      </div>
+    );
+  }
 };
 
 export default PriceButton;
