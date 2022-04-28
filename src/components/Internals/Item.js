f52b220cb98fb183250f511f9cbdef97c6e06b35
DvdHLee <dvdhyelee@gmail.com>
2022-04-28 16:35:47 -0700

    plate progress

--- src/components/Internals/Item.js
+++ src/components/Internals/Item.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Item.css";
 
 const Item = (props) => {
+  //"On state"
+  const [selected, setSelected] = useState(props.status);
+
+  //assigns correct amount of stars based on price value
+  var stars = [];
+  for (var i = 0; i < props.price; i++) {
+    stars.push(
+      <img
+        id="dollar"
+        src="/assets/Logos/dollar.png"
+        alt="this is a star icon"
+        key={i}
+      />
+    );
+  }
+
+  //Tells parent component to add item to list
+  const clickedAddToList = (event) => {
+    props.clickedaddlist(props.index);
+    setSelected(!selected);
+  };
+
+  //if on then invert colors
+  var itembutton = [];
+  if (selected == false) {
+    itembutton.push(<img src="/assets/Logos/addbutton.png" alt="this is an add button icon" key="1"/>);
+  } else {
+    itembutton.push(<img src="/assets/Logos/iaddbutton.png" className="invertedadd" alt="this is an add button icon" key="1"/>);
+  }
+
   return (
     <div className="item">
-      <button id="itembutton">
-        <img
-          src="/assets/Logos/addbutton.png"
-          alt="this is an add button icon"
-        />
+      <button id="itembutton" onClick={clickedAddToList}>
+        {itembutton}
       </button>
-      <p className="itemname">{props.name}</p>
       <div className="iteminfo">
+        <p className="itemname">{props.name}</p>
         <p className="itemrating">{props.rating}</p>
-        <img
-          id="star"
-          src="/assets/Logos/star.png"
-          alt="this is a star icon"
-        />
-        <img id="dollar" src="/assets/Logos/dollar.png" alt="this is a star icon"/>
+        <img id="star" src="/assets/Logos/star.png" alt="this is a star icon" />
+      </div>
+      <div className="iteminfo2">
+        <div className="allstars">{stars}</div>
+        {/* <p className="itemaddress">{props.address}</p> */}
       </div>
     </div>
   );
--- src/components/Plate.js
+++ src/components/Plate.js
@@ -1,28 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Plate.css";
-import Item from './Internals/Item';
+import Item from "./Internals/Item";
 
 const Plate = (props) => {
   console.log(props.resultsdata);
-  const itemname = "Test Restaurant";
-  const itemprice = 3;
-  const itemrating = 4.2;
+
+  //Adds or removes item to custom list
+  const [customlistindex, setCustomListIndex] = useState([]);
+  const clickedaddlist = (value) => {
+    if (customlistindex.indexOf(value) !== -1) {
+      setCustomListIndex((customlistindex) => customlistindex.splice(customlistindex.indexOf(value), 1));
+    } else {
+      setCustomListIndex((customlistindex) => [...customlistindex, value]);
+    }
+  };
+  console.log(customlistindex);
+
+  //Builds the custom list using the custom list index
+  var customlist = [];
+  if (customlistindex.length != 0) {
+    for (var i = 0; i < customlistindex.length; i++) {
+      customlist.push(
+        <Item
+          name={props.resultsdata[customlistindex[i]].name}
+          price={props.resultsdata[customlistindex[i]].price_level}
+          rating={props.resultsdata[customlistindex[i]].rating}
+          key={customlistindex[i]}
+          index={i}
+          clickedaddlist={clickedaddlist}
+          status={true}
+        ></Item>
+      );
+    }
+  }
+
+  //Builds the list
+  var resultslist = [];
+  for (var i = 0; i < props.resultsdata.length; i++) {
+    resultslist.push(
+      <Item
+        name={props.resultsdata[i].name}
+        price={props.resultsdata[i].price_level}
+        rating={props.resultsdata[i].rating}
+        key={i}
+        index={i}
+        clickedaddlist={clickedaddlist}
+        status={false}
+      ></Item>
+    );
+  }
+
+  //Toggles between list and custom list
+  var toggleCustomButton = [];
+
+  const [custom, setCustom] = useState(false);
+  const toggleCustom = (event) => {
+    setCustom(!custom);
+  };
+
+  if (custom == false) {
+    toggleCustomButton.push(
+      <img
+        src="/assets/Logos/forkandknife.png"
+        alt="this is fork and knife icon"
+        key="1"
+      />
+    );
+  } else {
+    toggleCustomButton.push(
+      <img
+        className="invertedfk"
+        src="/assets/Logos/iforkandknife.png"
+        alt="this is inverted fork and knife icon"
+        key="1"
+      />
+    );
+  }
+
   return (
-    <div className={props.showPlate ? "plate" : "disappear"}>{/* Ternary to pick className, disappear has a styling to display:none*/}
-      <p className="platetitle">ADD TO PLATE</p>
-      <button id="platebutton">
-        <img
-          src="/assets/Logos/forkandknife.png"
-          alt="this is fork and knife icon"
-        />
+    <div className={props.showPlate ? "plate" : "disappear"}>
+      {/* Ternary to pick className, disappear has a styling to display:none*/}
+      <button id="platebutton" onClick={toggleCustom}>
+        {toggleCustomButton}
       </button>
-      <div className="resultslist">
-        <Item name={itemname} price={itemprice} rating={itemrating}></Item>
-        <Item name={itemname} price={itemprice} rating={itemrating}></Item>
-        <Item name={itemname} price={itemprice} rating={itemrating}></Item>
-        <Item name={itemname} price={itemprice} rating={itemrating}></Item>
-      </div>
+      <p className={!custom ? "addplatetitle" : "disappear"}>ADD TO PLATE</p>
+      <p className={custom ? "platetitle" : "disappear"}>PLATE</p>
+      <div className={!custom ? "resultslist" : "disappear"}>{resultslist}</div>
+      <div className={custom ? "customlist" : "disappear"}>{customlist}</div>
     </div>
   );
 };
