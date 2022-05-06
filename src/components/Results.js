8de4c38545d597b2e7d8a021c850385209d57ee1
DvdHLee <dvdhyelee@gmail.com>
2022-05-06 01:47:18 -0700

    additional info optional commented out

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 import "./Results.css";
 
-function getDistanceFromLatLngInMiles(lat1, lon1, lat2, lon2) { //calculate distance between 2 latlng pairs
+function getDistanceFromLatLngInMiles(lat1, lng1, lat2, lng2) {
+  //calculate distance between 2 latlng pairs
   var R = 3963; // Radius of the earth in miles
-  var dLat = deg2rad(lat2 - lat1); // deg2rad below
-  var dLon = deg2rad(lon2 - lon1);
+  var dLat = deg2rad(lat2 - lat1);
+  var dLng = deg2rad(lng2 - lng1);
   var a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) *
       Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c; // Distance in miles
   return d;
@@ -27,23 +29,52 @@ const Results = (props) => {
     props.clickedChooseAgain();
   };
 
-  var photolink = "https://maps.googleapis.com/maps/api/place/photo?maxheight=400&maxwidth=400&photo_reference=";
+  var photolink =
+    "https://maps.googleapis.com/maps/api/place/photo?maxheight=400&maxwidth=400&photo_reference=";
   var placelink = "https://www.google.com/maps/dir/?api=1&origin=";
 
   if (props.resultsdata.length !== 0) {
-    photolink = photolink + props.resultsdata[props.chosennum].photos[0].photo_reference + "&sensor=false&key=AIzaSyA9oUuwu2IcJiytz70UxvzQIAtIWD_Pskc";
+    photolink =
+      photolink +
+      props.resultsdata[props.chosennum].photos[0].photo_reference +
+      "&sensor=false&key=AIzaSyA9oUuwu2IcJiytz70UxvzQIAtIWD_Pskc";
     placelink = placelink + props.origin[2] + "&destination=";
-    placelink = placelink + props.resultsdata[props.chosennum].vicinity.trim().replace(/ /g, "+").toString() + "&destination_place_id=";
+    placelink =
+      placelink +
+      props.resultsdata[props.chosennum].vicinity
+        .trim()
+        .replace(/ /g, "+")
+        .toString() +
+      "&destination_place_id=";
     placelink = placelink + props.resultsdata[props.chosennum].place_id;
   }
 
+  //Additional Information Retrieval
+  // var addInfoURL =
+  //   "https://maps.googleapis.com/maps/api/place/findplacefromtext/json" + 
+  //   "?fields=name%2Copening_hours%2Cphoto" + 
+  //   "&input=" +
+  //   props.resultsdata[props.chosennum].name.trim().replace(/ /g, "%20") +
+  //   "&inputtype=textquery" +
+  //   "&locationbias=circle%3A100%40" + 
+  //   props.resultsdata[props.chosennum].geometry.location.lat + 
+  //   "%2C" + 
+  //   props.resultsdata[props.chosennum].geometry.location.lng + 
+  //   "&key=AIzaSyA9oUuwu2IcJiytz70UxvzQIAtIWD_Pskc";
+
+  // useEffect(() => {
+  //   axios.get(addInfoURL).then((response) => {
+  //     console.log(response.data.candidates[0]);
+  //   });
+  // }, [addInfoURL]);
 
   if (props.resultsdata.length !== 0) {
     return (
-      <div className={props.showResults ? "results" : "disappear"}>{/* Ternary to pick className, disappear has a styling to display:none*/}
+      <div className={props.showResults ? "results" : "disappear"}>
+        {/* Ternary to pick className, disappear has a styling to display:none*/}
         <p className="letseat">LETS EAT!</p>
         <div className="placephotodiv">
-          <img className="placephoto" src={photolink} alt="this is a place"/>
+          <img className="placephoto" src={photolink} alt="this is a place" />
         </div>
         <p className="nameinfo">{props.resultsdata[props.chosennum].name}</p>
         <p className="addressinfo">
@@ -61,7 +92,14 @@ const Results = (props) => {
           Miles
         </p>
         <div className="resultbuttons">
-          <a href={placelink} className="golink" target="_blank" rel="noreferrer">GO</a>
+          <a
+            href={placelink}
+            className="golink"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GO
+          </a>
           <button className="chooseagain" onClick={clickedChooseAgain}>
             CHOOSE AGAIN
           </button>
@@ -69,10 +107,12 @@ const Results = (props) => {
       </div>
     );
   } else {
-    return <div className={props.showResults ? "noresults" : "disappear"}>
-      <p className="noresultstext">NO RESULTS</p>
-      <p>Tweak your filters!</p>
-      </div>;
+    return (
+      <div className={props.showResults ? "noresults" : "disappear"}>
+        <p className="noresultstext">NO RESULTS</p>
+        <p>Tweak your filters!</p>
+      </div>
+    );
   }
 };
 
