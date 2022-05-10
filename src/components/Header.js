be25341913f56c18dc07b2ead136e847bb7454d4
DvdHLee <dvdhyelee@gmail.com>
2022-05-09 23:11:14 -0700

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
 
-const Header = props => {
-    //Shows home when home button is clicked
-    const clickedHome = event => {
-        props.clickedHome();
-    }
+const Header = (props) => {
+  //Shows home when logo is clicked
+  const clickedHome = (event) => {
+    props.clickedHome();
+  };
 
-    //Shows plate when plate button is clicked
-    const clickedPlate = event => {
-        props.clickedPlate();
-    }
+  //Shows plate when plate button is clicked
+  const clickedPlate = (event) => {
+    props.clickedPlate();
+  };
 
-    //Goes back to Intro page when logo is clicked
-    const clickedLogo = event => {
-        props.clickedLogo();
-    }
-
-    return (
-        <div className='header'>
-            <div id="headertitle">
-                <button className="headerlogobutton" onClick={clickedLogo}>
-                    <img id="headerlogo" src="/assets/Logos/MealChewser_Logo6.png" alt="this is a egg shaped like a location pin with Meal Chewser"/>  
-                </button>      
-            </div>
-            <div id="headermenu">
-                <button className='headerbutton' onClick={clickedHome}> HOME </button>
-                <button className='headerbutton' onClick={clickedPlate}> PLATE </button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div id="headertitle">
+        <button className="headerlogobutton" onClick={clickedHome}>
+          <img
+            id="headerlogo"
+            src="/assets/Logos/MealChewser_Logo6.png"
+            alt="this is a egg shaped like a location pin with Meal Chewser"
+          />
+        </button>
+      </div>
+      <button className="headerplatebutton" onClick={clickedPlate}>
+        <img
+          src="/assets/Logos/iforkandknife.png"
+          alt="this is inverted fork and knife icon"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Header;
