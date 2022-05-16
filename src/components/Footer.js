3bcac72ab81db994dc7e04c7a1b7e71c71a14208
DvdHLee <dvdhyelee@gmail.com>
2022-05-16 13:24:51 -0700

    Instructions added: autocroll to top added on footer buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,36 @@ const Footer = (props) => {
   //Shows Results page when RESULTS is clicked
   const clickedResults = (event) => {
     props.clickedResults();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
   //Shows about page when ABOUT US is clicked
   const clickedAbout = (event) => {
     props.clickedAbout();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
   //Shows Home when HOME is clicked
   const clickedHome = (event) => {
     props.clickedHome();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
   //Shows Plate when PLATE is clicked
   const clickedPlate = (event) => {
     props.clickedPlate();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
   //Shows Instructions when INSTRUCTIONS is clicked
   const clickedInstructions = (event) => {
     props.clickedInstructions();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
   if (props.page === 0) {
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,21 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import './Instructions.css';
+import "./Instructions.css";
 
-const Instructions = props => {
-    //Make instructions disappear when backdrop is clicked
-    const clickedBackdrop = (event) => {
-        props.onClickedBackdrop();
-      };
+const Instructions = (props) => {
+  //Make instructions disappear when backdrop is clicked
+  const clickedBackdrop = (event) => {
+    props.onClickedBackdrop();
+  };
 
-    return (
-        <div className={props.showInstructions? "instructions" : "disappear"}>
-            <div className="backdrop" onClick={clickedBackdrop}></div>
-            <div className="instructioncontent">
-                <p>Hello!</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={props.showInstructions ? "instructions" : "disappear"}>
+      <div className="backdrop" onClick={clickedBackdrop}></div>
+      <div className="instruction">
+        <p className="instructionheader">WHAT IS MEAL CHEWSER?</p>
+        <p className="instructioncontent">
+          Meal Chewser is a random restaurant generator designed to make
+          deciding where to eat run and stress free. All you have to do is
+          simply type in your address, city, or zip code, and it will
+          automatically choose a restaurant for you. Don’t like the restaurant
+          that it chose? No problem! Just go to “Plate” and choose a couple of
+          restaurants to add to your plate and let it choose from there!
+        </p>
+        <p className="instructionheader">INSTRUCTIONS</p>
+        <p className="instructioncontent">
+          1. Type in address, city or zip code <br></br>
+          2. Edit “FILTERS” to help narrow down the choses to best match your
+          needs <br></br>
+          3. Click “CHEWS MY MEAL” to choose a random restaurant, or click
+          “BUILD MY PLATE” to select a few restaurants for the website to choose
+          from
+        </p>
+        <img
+          className="infoimg"
+          src="/assets/Logos/infoimg1.png"
+          alt="this is instruction image 1"
+        />
+        <p className="instructioncontent">
+          4. Click “GO” to get directions to the restaurant! Or click “CHOOSE
+          AGAIN” to choose another restaurant
+        </p>
+        <img
+          className="infoimg"
+          src="/assets/Logos/infoimg2.png"
+          alt="this is instruction image 2"
+        />
+        <p className="instructioncontent">
+          If you decide to “BUILD YOUR PLATE”: <br></br>
+          1. Add the restaurants that interest you <br></br>
+        </p>
+        <img
+          className="infoimg"
+          src="/assets/Logos/infoimg3.png"
+          alt="this is instruction image 3"
+        />
+        <p className="instructioncontent">
+          2. Click “GO TO PLATE” <br></br>
+          3. Click either “CHEWS MY MEAL” to choose your restaurant OR “ELIMINATE AN OPTION” to remove an option from the list!
+        </p>
+        <p className="instructionheader">ENJOY YOUR MEAL!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Instructions;
